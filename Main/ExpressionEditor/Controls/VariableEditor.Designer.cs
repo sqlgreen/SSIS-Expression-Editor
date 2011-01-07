a0994a717f6b1f0be75259c1ccadbc0f977e7148
DarrenSQLIS_cp <27521569+sqlgreen@users.noreply.github.com>
2011-01-07 17:18:34 +0000

    Fixed variable editor dialog, bug with higher DPI. Upgraded StyleCop ref to 4.4

--- Main/ExpressionEditor/Controls/VariableEditor.Designer.cs
+++ Main/ExpressionEditor/Controls/VariableEditor.Designer.cs
@@ -43,7 +43,6 @@ namespace Konesans.Dts.ExpressionEditor.Controls
             this.comboBoxValue = new System.Windows.Forms.ComboBox();
             this.numericUpDownValue = new System.Windows.Forms.NumericUpDown();
             this.dateTimePickerValue = new System.Windows.Forms.DateTimePicker();
-            this.buttonTextExpand = new System.Windows.Forms.Button();
             this.labelNamespace = new System.Windows.Forms.Label();
             this.textBoxNamespace = new System.Windows.Forms.TextBox();
             this.line3D1 = new Konesans.Dts.ExpressionEditor.Controls.Line3D();
@@ -54,9 +53,10 @@ namespace Konesans.Dts.ExpressionEditor.Controls
             // 
             this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(239, 181);
+            this.buttonCancel.Location = new System.Drawing.Point(319, 223);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.Size = new System.Drawing.Size(100, 28);
             this.buttonCancel.TabIndex = 7;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
@@ -65,9 +65,10 @@ namespace Konesans.Dts.ExpressionEditor.Controls
             // 
             this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.buttonOK.Location = new System.Drawing.Point(158, 182);
+            this.buttonOK.Location = new System.Drawing.Point(211, 224);
+            this.buttonOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonOK.Name = "buttonOK";
-            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.Size = new System.Drawing.Size(100, 28);
             this.buttonOK.TabIndex = 6;
             this.buttonOK.Text = "OK";
             this.buttonOK.UseVisualStyleBackColor = true;
@@ -78,9 +79,10 @@ namespace Konesans.Dts.ExpressionEditor.Controls
             this.labelName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.labelName.AutoSize = true;
-            this.labelName.Location = new System.Drawing.Point(12, 24);
+            this.labelName.Location = new System.Drawing.Point(16, 30);
+            this.labelName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(38, 13);
+            this.labelName.Size = new System.Drawing.Size(49, 17);
             this.labelName.TabIndex = 5;
             this.labelName.Text = "Name:";
             // 
@@ -88,9 +90,10 @@ namespace Konesans.Dts.ExpressionEditor.Controls
             // 
             this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxName.Location = new System.Drawing.Point(84, 21);
+            this.textBoxName.Location = new System.Drawing.Point(112, 26);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(229, 20);
+            this.textBoxName.Size = new System.Drawing.Size(304, 22);
             this.textBoxName.TabIndex = 1;
             this.textBoxName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextKeyDown);
             // 
@@ -99,9 +102,10 @@ namespace Konesans.Dts.ExpressionEditor.Controls
             this.labelType.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.labelType.AutoSize = true;
-            this.labelType.Location = new System.Drawing.Point(13, 94);
+            this.labelType.Location = new System.Drawing.Point(17, 116);
+            this.labelType.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelType.Name = "labelType";
-            this.labelType.Size = new System.Drawing.Size(34, 13);
+            this.labelType.Size = new System.Drawing.Size(44, 17);
             this.labelType.TabIndex = 7;
             this.labelType.Text = "Type:";
             // 
@@ -111,9 +115,10 @@ namespace Konesans.Dts.ExpressionEditor.Controls
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBoxType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxType.FormattingEnabled = true;
-            this.comboBoxType.Location = new System.Drawing.Point(85, 93);
+            this.comboBoxType.Location = new System.Drawing.Point(113, 114);
+            this.comboBoxType.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxType.Name = "comboBoxType";
-            this.comboBoxType.Size = new System.Drawing.Size(229, 21);
+            this.comboBoxType.Size = new System.Drawing.Size(304, 24);
             this.comboBoxType.TabIndex = 3;
             this.comboBoxType.SelectedIndexChanged += new System.EventHandler(this.ComboBoxType_SelectedIndexChanged);
             // 
@@ -124,11 +129,13 @@ namespace Konesans.Dts.ExpressionEditor.Controls
             this.textBoxValue.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxValue.Location = new System.Drawing.Point(85, 129);
+            this.textBoxValue.Location = new System.Drawing.Point(113, 159);
+            this.textBoxValue.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxValue.Multiline = true;
             this.textBoxValue.Name = "textBoxValue";
-            this.textBoxValue.Size = new System.Drawing.Size(229, 20);
+            this.textBoxValue.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxValue.Size = new System.Drawing.Size(304, 20);
             this.textBoxValue.TabIndex = 4;
-            this.textBoxValue.VisibleChanged += new System.EventHandler(this.TextBoxValue_VisibleChanged);
             this.textBoxValue.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextKeyDown);
             // 
             // labelValue
@@ -136,9 +143,10 @@ namespace Konesans.Dts.ExpressionEditor.Controls
             this.labelValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.labelValue.AutoSize = true;
-            this.labelValue.Location = new System.Drawing.Point(13, 130);
+            this.labelValue.Location = new System.Drawing.Point(17, 160);
+            this.labelValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelValue.Name = "labelValue";
-            this.labelValue.Size = new System.Drawing.Size(37, 13);
+            this.labelValue.Size = new System.Drawing.Size(48, 17);
             this.labelValue.TabIndex = 10;
             this.labelValue.Text = "Value:";
             // 
@@ -151,18 +159,20 @@ namespace Konesans.Dts.ExpressionEditor.Controls
             this.comboBoxValue.Items.AddRange(new object[] {
             "False",
             "True"});
-            this.comboBoxValue.Location = new System.Drawing.Point(85, 129);
+            this.comboBoxValue.Location = new System.Drawing.Point(113, 159);
+            this.comboBoxValue.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxValue.Name = "comboBoxValue";
-            this.comboBoxValue.Size = new System.Drawing.Size(229, 21);
+            this.comboBoxValue.Size = new System.Drawing.Size(304, 24);
             this.comboBoxValue.TabIndex = 12;
             // 
             // numericUpDownValue
             // 
             this.numericUpDownValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.numericUpDownValue.Location = new System.Drawing.Point(85, 129);
+            this.numericUpDownValue.Location = new System.Drawing.Point(113, 159);
+            this.numericUpDownValue.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDownValue.Name = "numericUpDownValue";
-            this.numericUpDownValue.Size = new System.Drawing.Size(229, 20);
+            this.numericUpDownValue.Size = new System.Drawing.Size(304, 22);
             this.numericUpDownValue.TabIndex = 4;
             // 
             // dateTimePickerValue
@@ -171,33 +181,22 @@ namespace Konesans.Dts.ExpressionEditor.Controls
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.dateTimePickerValue.CustomFormat = "dd MMM yyyy HH:mm:ss";
             this.dateTimePickerValue.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePickerValue.Location = new System.Drawing.Point(85, 129);
+            this.dateTimePickerValue.Location = new System.Drawing.Point(113, 159);
+            this.dateTimePickerValue.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePickerValue.Name = "dateTimePickerValue";
-            this.dateTimePickerValue.Size = new System.Drawing.Size(229, 20);
+            this.dateTimePickerValue.Size = new System.Drawing.Size(304, 22);
             this.dateTimePickerValue.TabIndex = 4;
             this.dateTimePickerValue.Value = new System.DateTime(2005, 12, 11, 0, 10, 0, 0);
-            // 
-            // buttonTextExpand
-            // 
-            this.buttonTextExpand.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonTextExpand.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonTextExpand.Image = global::Konesans.Dts.ExpressionEditor.Properties.Resources.GoLtr;
-            this.buttonTextExpand.Location = new System.Drawing.Point(295, 129);
-            this.buttonTextExpand.Name = "buttonTextExpand";
-            this.buttonTextExpand.Size = new System.Drawing.Size(20, 20);
-            this.buttonTextExpand.TabIndex = 5;
-            this.buttonTextExpand.UseVisualStyleBackColor = true;
-            this.buttonTextExpand.Visible = false;
-            this.buttonTextExpand.Click += new System.EventHandler(this.ButtonTextExpand_Click);
             // 
             // labelNamespace
             // 
             this.labelNamespace.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.labelNamespace.AutoSize = true;
-            this.labelNamespace.Location = new System.Drawing.Point(13, 60);
+            this.labelNamespace.Location = new System.Drawing.Point(17, 74);
+            this.labelNamespace.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelNamespace.Name = "labelNamespace";
-            this.labelNamespace.Size = new System.Drawing.Size(67, 13);
+            this.labelNamespace.Size = new System.Drawing.Size(87, 17);
             this.labelNamespace.TabIndex = 16;
             this.labelNamespace.Text = "Namespace:";
             // 
@@ -205,9 +204,10 @@ namespace Konesans.Dts.ExpressionEditor.Controls
             // 
             this.textBoxNamespace.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxNamespace.Location = new System.Drawing.Point(84, 57);
+            this.textBoxNamespace.Location = new System.Drawing.Point(112, 70);
+            this.textBoxNamespace.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxNamespace.Name = "textBoxNamespace";
-            this.textBoxNamespace.Size = new System.Drawing.Size(229, 20);
+            this.textBoxNamespace.Size = new System.Drawing.Size(304, 22);
             this.textBoxNamespace.TabIndex = 2;
             this.textBoxNamespace.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextKeyDown);
             // 
@@ -216,23 +216,23 @@ namespace Konesans.Dts.ExpressionEditor.Controls
             this.line3D1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.line3D1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.line3D1.Location = new System.Drawing.Point(0, 167);
-            this.line3D1.MaximumSize = new System.Drawing.Size(12400, 2);
-            this.line3D1.MinimumSize = new System.Drawing.Size(272, 2);
+            this.line3D1.Location = new System.Drawing.Point(0, 206);
+            this.line3D1.Margin = new System.Windows.Forms.Padding(4);
+            this.line3D1.MaximumSize = new System.Drawing.Size(16533, 2);
+            this.line3D1.MinimumSize = new System.Drawing.Size(363, 2);
             this.line3D1.Name = "line3D1";
-            this.line3D1.Size = new System.Drawing.Size(334, 2);
+            this.line3D1.Size = new System.Drawing.Size(445, 2);
             this.line3D1.TabIndex = 11;
             // 
             // VariableEditor
             // 
             this.AcceptButton = this.buttonOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(326, 217);
+            this.ClientSize = new System.Drawing.Size(435, 276);
             this.Controls.Add(this.labelNamespace);
             this.Controls.Add(this.textBoxNamespace);
-            this.Controls.Add(this.buttonTextExpand);
             this.Controls.Add(this.line3D1);
             this.Controls.Add(this.labelValue);
             this.Controls.Add(this.labelName);
@@ -245,9 +245,10 @@ namespace Konesans.Dts.ExpressionEditor.Controls
             this.Controls.Add(this.textBoxValue);
             this.Controls.Add(this.dateTimePickerValue);
             this.Controls.Add(this.numericUpDownValue);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(334, 251);
+            this.MinimumSize = new System.Drawing.Size(443, 302);
             this.Name = "VariableEditor";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
@@ -273,7 +274,6 @@ namespace Konesans.Dts.ExpressionEditor.Controls
         private System.Windows.Forms.ComboBox comboBoxValue;
         private System.Windows.Forms.NumericUpDown numericUpDownValue;
         private System.Windows.Forms.DateTimePicker dateTimePickerValue;
-        private System.Windows.Forms.Button buttonTextExpand;
         private System.Windows.Forms.Label labelNamespace;
         private System.Windows.Forms.TextBox textBoxNamespace;
     }
